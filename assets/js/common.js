9cb0da1941575a2d0c7247e5cf44031bff592649
Nathan-Gouin <nathan.gouin@univ-lehavre.fr>
2025-01-13 18:24:30 +0100

    mise a jour de common.js

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,6 +1,5 @@
 "use strict";
-function __export(m) {
-    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
-}
-__export(require('./src/commonComponent.component'));
-//# sourceMappingURL=common.js.map
+
+import * as commonComponent from './src/commonComponent.component';
+
+export default commonComponent;
