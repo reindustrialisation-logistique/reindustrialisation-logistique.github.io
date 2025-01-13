cbafd2ffe1b336fbbdd56c2d8bcedc4807f1be5d
Nathan-Gouin <nathan.gouin@univ-lehavre.fr>
2025-01-13 17:15:35 +0100

    Correction de require pour navigateur dans common.js

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,6 +1,6 @@
-"use strict";
-function __export(m) {
-    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
-}
-__export(require('./src/commonComponent.component'));
-//# sourceMappingURL=common.js.map
+<script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+<script>
+    $(document).ready(function () {
+        console.log('Page is ready!');
+    });
+</script>
