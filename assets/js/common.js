a1cbee58d1815120fc28718f221b3d1166988d2a
Nathan-Gouin <nathan.gouin@univ-lehavre.fr>
2025-01-14 13:24:07 +0100

    mise a jour

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,12 +1,26 @@
 "use strict";
 
-// Vérifiez si l'objet `commonComponent` est déjà défini dans l'environnement global
-if (typeof window !== "undefined" && window.commonComponent) {
-    // Utilisez directement l'objet `commonComponent` global
-    const commonComponent = window.commonComponent;
+// Importer le composant depuis commonComponent.js
+import generateCommonComponent from "./commonComponent.js";
 
-    // Exemple d'utilisation :
-    console.log("Common Component chargé :", commonComponent);
-} else {
-    console.error("Erreur : commonComponent n'est pas défini. Vérifiez le fichier src/commonComponent.js");
-}
+// Exemple d'utilisation : données fictives
+const componentData = {
+    title: "Titre du Projet",
+    description: "Description du projet ici.",
+    navigation: [
+        { url: "/home", label: "Accueil" },
+        { url: "/about", label: "À propos" },
+    ],
+    contentBlocks: [
+        { image: "/assets/images/team1.jpg", title: "Membre 1", description: "Description 1" },
+        { image: "/assets/images/team2.jpg", title: "Membre 2", description: "Description 2" },
+    ],
+    sponsors: [
+        { logo: "/assets/images/sponsor1.png", name: "Sponsor 1" },
+        { logo: "/assets/images/sponsor2.png", name: "Sponsor 2" },
+    ],
+    siteTitle: "Nom du Site",
+};
+
+// Générer le composant et l'insérer dans l'élément avec l'ID "app"
+document.getElementById("app").innerHTML = generateCommonComponent(componentData);
--- assets/js/src/main.js
+++ assets/js/src/main.js
@@ -1,5 +1,5 @@
-import generateCommonComponent from "./commonComponent.js";
-import componentData from "./componentData.js";
+import generateCommonComponent from ".assets/js/commonComponent.js";
+import componentData from ".assets/js/componentData.js";
 
 // Ajoutez le contenu généré dans l'élément cible
 document.getElementById("app").innerHTML = generateCommonComponent(componentData);
